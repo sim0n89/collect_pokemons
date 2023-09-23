856ac525d1c3cdcdd7234306545375713d0d5a48
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-09-23 10:18:15 +0300

    форматирование flake8

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -6,9 +6,9 @@ from .models import PokemonEntity, Pokemon
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
-    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
-    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
-    '&fill=transparent'
+    "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision"
+    "/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832"
+    "&fill=transparent"
 )
 
 
@@ -31,8 +31,7 @@ def show_all_pokemons(request):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     time_now = timezone.now()
     pokemon_entities = PokemonEntity.objects.filter(
-        appeared_at__lte = time_now,
-        disappeared_at__gte = time_now 
+        appeared_at__lte=time_now, disappeared_at__gte=time_now
     )
 
     for pokemon_entity in pokemon_entities:
@@ -56,17 +55,22 @@ def show_all_pokemons(request):
             image_url = request.build_absolute_uri(pokemon_image.url)
         else:
             image_url = DEFAULT_IMAGE_URL
-        pokemons_on_page.append({
-            'pokemon_id': pokemon.id,
-            'img_url': image_url,
-            'title_ru': pokemon.title,
-        })
+        pokemons_on_page.append(
+            {
+                "pokemon_id": pokemon.id,
+                "img_url": image_url,
+                "title_ru": pokemon.title,
+            }
+        )
 
-
-    return render(request, 'mainpage.html', context={
-        'map': folium_map._repr_html_(),
-        'pokemons': pokemons_on_page,
-    })
+    return render(
+        request,
+        "mainpage.html",
+        context={
+            "map": folium_map._repr_html_(),
+            "pokemons": pokemons_on_page,
+        },
+    )
 
 
 def show_pokemon(request, pokemon_id):
@@ -86,12 +90,11 @@ def show_pokemon(request, pokemon_id):
     )
 
     for pokemon_entity in pokemon_entities:
-        add_pokemon(
-            folium_map,
-            pokemon_entity.lat,
-            pokemon_entity.lon,
-            image_url
-        )
+        add_pokemon(folium_map,
+                    pokemon_entity.lat,
+                    pokemon_entity.lon,
+                    image_url
+                    )
 
     previous_evolution = {}
     previous_pokemon = pokemon.previous_evolution
@@ -100,9 +103,9 @@ def show_pokemon(request, pokemon_id):
             "title_ru": previous_pokemon.title,
             "pokemon_id": previous_pokemon.id,
             "img_url": request.build_absolute_uri(previous_pokemon.image.url),
-            'description': previous_pokemon.description,
-            'title_en': previous_pokemon.title_en,
-            'title_jp': previous_pokemon.title_jp,
+            "description": previous_pokemon.description,
+            "title_en": previous_pokemon.title_en,
+            "title_jp": previous_pokemon.title_jp,
         }
 
     next_evolution = {}
@@ -111,19 +114,23 @@ def show_pokemon(request, pokemon_id):
         next_evolution = {
             "title_ru": next_pokemon.title,
             "pokemon_id": next_pokemon.id,
-            "img_url": request.build_absolute_uri(next_pokemon.image.url)
+            "img_url": request.build_absolute_uri(next_pokemon.image.url),
         }
 
-        
-    return render(request, 'pokemon.html', context={
-        'map': folium_map._repr_html_(), 'pokemon': {
-            'pokemon_id': pokemon.id,
-            'img_url': image_url,
-            'title_ru': pokemon.title,
-            'description': pokemon.description,
-            'title_en': pokemon.title_en,
-            'title_jp': pokemon.title_jp,
-            'previous_evolution': previous_evolution,
-            'next_evolution': next_evolution,
-        }
-    })
+    return render(
+        request,
+        "pokemon.html",
+        context={
+            "map": folium_map._repr_html_(),
+            "pokemon": {
+                "pokemon_id": pokemon.id,
+                "img_url": image_url,
+                "title_ru": pokemon.title,
+                "description": pokemon.description,
+                "title_en": pokemon.title_en,
+                "title_jp": pokemon.title_jp,
+                "previous_evolution": previous_evolution,
+                "next_evolution": next_evolution,
+            },
+        },
+    )
